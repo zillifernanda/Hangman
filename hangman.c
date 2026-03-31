2ad75dbb7ff62443cdf9c3d0d1025fdbd09544b6
Fernanda Zilli <zilli_1984@hotmail.com>
2026-03-31 12:21:44 +0200

    main

--- hangman.c
+++ hangman.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
+int main() {
+    char secretword[20];
+    sprintf(secretword, "Melancia");
+
+    int won = 0;
+    int lost = 0;
+
+    do{
+        char try;
+        scanf("%c", &try);
+
+        for(int i = 0; i < strlen(secretword); i++)
+            if(secretword[i] == try) {
+                printf("The position %d has this letter!\n", i);
+            }
+
+
+
+    } while (!won && !lost);
+    
+}
